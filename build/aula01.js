cd3fb435e6f2b9289cb7771a366deba6a5315c73
Joaquim Silva <joaquimsilvaneto@gmail.com>
2024-10-29 16:54:53 -0300

    initial commit

---
+++ build/aula01.js
@@ -0,0 +1,11 @@
+"use strict";
+let vteste;
+vteste = "Gain";
+vteste = "2024";
+let cursos = ["React", "Angular", "Vue"];
+let valores = [1, 2, 3, 4, 5];
+cursos.push("Node");
+valores.push(6);
+console.log(cursos);
+console.log(valores);
+console.log(vteste);
---
+++ src/aula01.ts
@@ -0,0 +1,14 @@
+// let vteste: string | number;
+let vteste: any;
+vteste = "Gain";
+vteste = "2024";
+
+let cursos = ["React", "Angular", "Vue"];
+let valores = [1, 2, 3, 4, 5];
+
+cursos.push("Node");
+valores.push(6);
+
+console.log(cursos);
+console.log(valores);
+console.log(vteste);
